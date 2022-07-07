0306d463e2d056d3e715df7170f7c48c333edaa0
pritamcoder <107984522+pritamcoder@users.noreply.github.com>
2022-07-07 11:38:05 +0530

    Add files via upload
    education wabsite

---
+++ script.js
@@ -0,0 +1,10 @@
+let navbar=document.getElementById('nav')
+let ml1=document.getElementById('button')
+
+
+
+function anime(){
+    navbar.classList.toggle('anime')
+    ml1.classList.toggle('chang')
+
+}
